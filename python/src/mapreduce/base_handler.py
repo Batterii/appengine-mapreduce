05f6f0359ad0169f10ef12da8d43721a4d326897
Nick Franceschina <nick@batterii.com>
2016-08-02 22:33:35 -0400

    [nazYbg1Y] migration bug... shouldn't have been monkeying with the namespace in the middle of the pipeline (_transient_data_cleanup).... BUT, while I was at it I A) added a map-reduce step to the migrations/unit_test...  B) got it working via the test framework... C) uncovered an issue where the test framework wasn't properly setting the namespace before running each task.   added some extra checks in the pipeline/mapreduce customizations to make sure the namespace doesn't get accidentally overridden

--- python/src/mapreduce/base_handler.py
+++ python/src/mapreduce/base_handler.py
@@ -213,10 +213,10 @@ class JsonHandler(webapp.RequestHandler):
           403, message="Got JSON request with no X-Requested-With header")
       return
 
-    #NPF ADDED
-    ns = self.request.get('ns')
-    from google.appengine.api import namespace_manager
-    if ns: namespace_manager.set_namespace(ns)
+    # NPF ADDED - only if this isn't being run by a task and only if the &ns= param is provided
+    if 'HTTP_X_APPENGINE_TASKNAME' not in self.request.environ and self.request.get('ns'):
+      from google.appengine.api import namespace_manager
+      namespace_manager.set_namespace(self.request.get('ns'))
 
     self.json_response.clear()
     try:
